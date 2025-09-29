aa4721fc0ab8956307a8b1a63a4bd25cf39d54d6
srhdp <srhdp@BRN-MacBook-Pro.local>
2025-09-29 14:43:38 +0700

    card command

--- cards/src/main/java/com/eazybytes/cards/repository/CardsRepository.java
+++ cards/src/main/java/com/eazybytes/cards/repository/CardsRepository.java
@@ -10,5 +10,6 @@ import java.util.Optional;
 public interface CardsRepository extends JpaRepository<Cards, Long> {
 
     Optional<Cards> findByMobileNumberAndActiveSw(String mobileNumber, boolean activeSw);
+    Optional<Cards> findByCardNumberAndActiveSw(Long cardNumber, boolean activeSw);
 
 }
